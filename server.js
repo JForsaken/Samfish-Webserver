93c868dc011946471540aac1487e9003490536a9
Justin D'Errico <justin.derrico1991@gmail.com>
2016-08-26 02:36:29 -0400

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 /* CONFIGS */
+app.use(cors());
 app.use(bodyParser.json({
   limit: '50mb',
 }));
