37f7a0e075ea38239779f8c882d5498355e5a358
Justin D'Errico <justin.derrico1991@gmail.com>
2016-08-29 22:33:05 -0400

    heroku db creds

--- app/constants/database.js
+++ app/constants/database.js
@@ -1,5 +1,6 @@
 // Use your local psql connection string at the end of this line
-exports.CONNECTION_STRING = 'postgres://dba:1vqN4Pus@localhost:5432/samfish';
+exports.CONNECTION_STRING = 'postgres://kqaufaedjlzjsm:c4AbCFAcWe3s53X0S-qSkFQS6m@ec2-54-243-235-107.compute-1.amazonaws.com:5432/dc9ilfs18v9s11';
+
 
 exports.ORDER_STYLE = {
   DESC: 'DESC',
